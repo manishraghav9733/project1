8548d31810d695fed2f70ea1ffacc220dff3d6ec
Manish Raghav <manishraghav9733@gmail.com>
2020-01-23 14:37:25 +0530

    final change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>manish raghav</h1>
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
+ const App=() =>{
+   const [list, setList]= useState([""]);
+ 
+   useEffect(()=>{
+    const fetchData=async () =>{
+      const response= await axios.get(
+        'https://bizeminence.com/yougee/api/test/get_data'
+      ,{
+        headers:{
+       'Access-Control-Allow-Origin': true,
+     },
+   });
+      setList(response.data);
+      console.log(response.data);
+    };
+     fetchData();
+   },[]); 
+   return(
+   <div>
+     {
+          list.map((item,i) => (
+              <div key={i}>
+              <span>name - {item.name}</span>
+              <span> address - {item.address}</span>
+              <span>contact - {item.contact}</span>
+              </div>
+          ))
+      }
     </div>
-  );
+);
 }
-
 export default App;
---
+++ src/api.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://bizeminence.com/yougee/api/test/get_data'
+});
