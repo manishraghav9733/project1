b977ebb9372a453fdaa0247ac3a0f0614dc38073
Manish Raghav <manishraghav9733@gmail.com>
2020-01-24 15:39:27 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios';
+
+
  const App=() =>{
-   const [list, setList]= useState([""]);
- 
-   useEffect(()=>{
-    const fetchData=async () =>{
-      const response= await axios.get(
-        'https://bizeminence.com/yougee/api/test/get_data'
-      ,{
-        headers:{
-       'Access-Control-Allow-Origin': true,
-     },
-   });
-      setList(response.data);
-      console.log(response.data);
+  const [resource, setData]=useState([]);
+   const fetchData = async () => {
+    const response= await axios.get(
+      "http://dummy.restapiexample.com/api/v1/employees");
+      setData(response);
+      console.log(response.data); 
     };
-     fetchData();
-   },[]); 
-   return(
-   <div>
-     {
-          list.map((item,i) => (
+  useEffect(() => {
+     fetchData(resource);
+  },[]);
+
+  const list = ()=>resource.map((item,i) => (
               <div key={i}>
               <span>name - {item.name}</span>
               <span> address - {item.address}</span>
               <span>contact - {item.contact}</span>
               </div>
           ))
-      }
-    </div>
-);
+
+return(<div>
+  [list]
+  </div>
+  );
 }
 export default App;
