200004c74d4e1094c9f1e4faa19c7befa19d93b6
videsh <videsh.ghodarop@iaugmentor.com>
2020-01-24 15:13:21 +0530

    bugs fixed of data not visible

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Table } from "antd";
+import "antd/dist/antd.css";
 
+const App = () => {
+  const [resource, setResource] = useState([]);
+  const fetchData = async () => {
+    const response = await axios.get(
+      "http://dummy.restapiexample.com/api/v1/employees"
+    );
+    setResource(response.data.data);
+    console.log(response.data.data);
+  };
 
- const App=() =>{
-  const [resource, setData]=useState([]);
-   const fetchData = async () => {
-    const response= await axios.get(
-      "http://dummy.restapiexample.com/api/v1/employees");
-      setData(response);
-      console.log(response.data); 
-    };
   useEffect(() => {
-     fetchData(resource);
-  },[]);
+    fetchData(resource);
+  }, []);
 
-  const list = ()=>resource.map((item,i) => (
-              <div key={i}>
-              <span>name - {item.name}</span>
-              <span> address - {item.address}</span>
-              <span>contact - {item.contact}</span>
-              </div>
-          ))
+  const columns = [
+    {
+      title: "Id",
+      dataIndex: "id",
+      key: "id"
+    },
+    {
+      title: "Name",
+      dataIndex: "employee_name",
+      key: "employee_name"
+    },
+    {
+      title: "Salary",
+      dataIndex: "employee_salary",
+      key: "employee_salary"
+    }
+  ];
 
-return(<div>
-  {list}
-  </div>
+  return (
+    <div>
+      <Table columns={columns} dataSource={resource} rowKey={row => row.id} />
+    </div>
   );
-}
+};
 export default App;
