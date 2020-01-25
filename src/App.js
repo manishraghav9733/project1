928225a4f1dd73557cb00244c91889823ac3083b
Manish Raghav <manishraghav9733@gmail.com>
2020-01-25 16:16:41 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,44 @@
-import React, {useState, useEffect} from 'react'
-import { Table } from "antd"
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Table } from "antd";
+import "antd/dist/antd.css";
 
+const App = () => {
+  const [resource, setResource] = useState([]);
+  const fetchData = async () => {
+    const response = await axios.get(
+      "http://dummy.restapiexample.com/api/v1/employees"
+    );
+    setResource(response.data.data);
+    console.log(response.data.data);
+  };
 
- const App=() =>{
-  const [resource, setData]=useState([]);
-   const fetchData = async () => {
-    const response= await axios.get(
-      "http://dummy.restapiexample.com/api/v1/employees");
-        setData(JSON.parse(response.data['id', 'employee_name', 'employee_salary','Title']));
-      console.log(JSON.parse(response.data));
-    };
   useEffect(() => {
-     fetchData();
-  },[]);
+    fetchData(resource);
+  }, []);
 
-const columns = [
-  {
-    title: 'id',
-    dataIndex: 'id',
-    key: 'id',
-  },
-  {
-    title: 'employee_name',
-    dataIndex: 'employee_name',
-    key: 'employee_name',
-  },
-  {
-    title: 'employee_salary',
-    dataIndex: 'employee_salary',
-    key: 'employee_salary',
-  },
-  {
-    title: 'Title',
-    dataIndex: 'Title',
-    key: 'Title',
-  },
-];
-//manish
-  return(
-    <Table 
-    dataSource={resource} 
-    columns={columns} 
-    />
-    );    
-}
+  const columns = [
+    {
+      title: "Id",
+      dataIndex: "id",
+      key: "id"
+    },
+    {
+      title: "Name",
+      dataIndex: "employee_name",
+      key: "employee_name"
+    },
+    {
+      title: "Salary",
+      dataIndex: "employee_salary",
+      key: "employee_salary"
+    }
+  ];
 
-
+  return (
+    <div>
+      <Table columns={columns} dataSource={resource} rowKey={row => row.id} />
+    </div>
+  );
+};
 export default App;
